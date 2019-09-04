9bd9895f8b1b7ee56a615903ca0ea04a03a646be
ingrwf <ingrwf@cpg-453071.cepegra2008.lan>
2019-09-04 16:34:57 +0200

    day1 commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 import React from "react";
+import Axios from "axios";
+
+// App components
+
+//App context
 
 export default function App() {
-  return <div className="App">hello</div>;
+  const userlogin = "";
+  const userpass = "";
+
+  return (
+    <div className="App">
+      <h1>Bienvenue&nbsp;!</h1>
+      <form>
+        <p>
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" />
+        </p>
+        <p>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" />
+        </p>
+        <input type="submit" value="Se connecter" />
+      </form>
+    </div>
+  );
 }
