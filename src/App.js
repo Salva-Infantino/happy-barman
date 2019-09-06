c6246e584ababa533b22c69a6f5a65e194eb02da
ingrwf <ingrwf@cpg-453071.cepegra2008.lan>
2019-09-06 16:18:00 +0200

    day3 commit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,77 @@
-import React from "react";
-//import Axios from "axios";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, { useState } from "react";
+import axios from "axios";
 
 // App components
 import Client from "./components/client";
 import Barman from "./components/barman";
 
-//App context
-
-export default function App(props) {
-  //const userlogin = "";
-  //const userpass = "";
-
-  const clickLogin = ev => {
+export default function App() {
+  const [userLogin, setuserLogin] = useState();
+  const [userPass, setuserPass] = useState();
+  const [isLogged, setisLogged] = useState(false);
+  const [currentUser, setCurrentUser] = useState({
+    userid: "",
+    username: "",
+    role: "",
+    islogged: false
+  });
+  //rq api
+  const handleSubmit = ev => {
     ev.preventDefault();
+    axios
+      .post("http://localhost:8080/auth/local", {
+        identifier: userLogin,
+        password: userPass
+      })
+      .then(response => {
+        setisLogged(true);
+        const user = response.data.user;
+        const token = response.data.jwt;
+        //console.log(user);
+        setCurrentUser({
+          userid: user.id,
+          username: user.username,
+          role: user.role.id,
+          islogged: true
+        });
+      })
+      .catch(error => {
+        console.log("An error occured:", error);
+      });
+  };
+
+  const handleUserName = ev => {
+    setuserLogin(ev.currentTarget.value);
+  };
+
+  const handleUserPass = ev => {
+    setuserPass(ev.currentTarget.value);
   };
 
   return (
     <div className="App">
-      <Router>
-        <ul>
-          <li>
-            <Link to="/">Authentification</Link>
-          </li>
-          <li>
-            <Link to="/client">client</Link>
-          </li>
-          <li>
-            <Link to="/barman">barman</Link>
-          </li>
-        </ul>
-        <Switch>
-          <Route exact path="/">
-            <h1>Bienvenue&nbsp;!</h1>
-            <form onSubmit={clickLogin}>
-              <p>
-                <label htmlFor="username">Username</label>
-                <input type="text" id="username" />
-              </p>
-              <p>
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" />
-              </p>
-              <input type="submit" value="Se connecter" />
-            </form>
-          </Route>
-          <Route exact path="/client">
-            <Client />
-          </Route>
-          <Route exact path="/barman">
-            <Barman />
-          </Route>
-        </Switch>
-      </Router>
+      {isLogged ? (
+        currentUser.role === 2 ? (
+          <Client />
+        ) : (
+          <Barman />
+        )
+      ) : (
+        <>
+          <h1>Bienvenue&nbsp;!</h1>
+          <form onSubmit={handleSubmit}>
+            <p>
+              <label htmlFor="username">Username</label>
+              <input type="text" id="username" onChange={handleUserName} />
+            </p>
+            <p>
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" onChange={handleUserPass} />
+            </p>
+            <input type="submit" value="Se connecter" />
+          </form>
+        </>
+      )}
     </div>
   );
 }
--- src/components/client.js
+++ src/components/client.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
 //import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
@@ -8,21 +8,55 @@ import Axios from "axios";
 
 export default function Client() {
   //rq api
-  const [beers, setbeers] = useState;
+  const [beers, setbeers] = useState([]);
+  const [neworder, setneworder] = useState([]);
 
-  Axios.get("http://localhost:8080/Beers/")
-    .then(function(response) {
-      setbeers(response.data);
-      console.log(setbeers(response.data));
-    })
-    .catch(error => {
-      console.log(error);
-    });
+  useEffect(() => {
+    Axios.get("http://localhost:8080/Beers/")
+      .then(function(response) {
+        setbeers(response.data);
+        //console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  const addOrder = ev => {
+    ev.preventDefault();
+  };
 
   return (
     <div className="Client">
       <h1>Vous êtes CLIENT</h1>
-      <ul></ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Nom</th>
+            <th>Catégorie</th>
+            <th>Alcool</th>
+            <th>Prix</th>
+          </tr>
+        </thead>
+        <tbody>
+          {beers.map(b => (
+            <tr key={b.id}>
+              <td>{b.name}</td>
+              <td>{b.category}</td>
+              <td>{b.alcool}%</td>
+              <td>{b.price}€</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <hr />
+      <form></form>
+      <hr />
+      <div className="myOrder">
+        <table>
+          <caption>Ma commande</caption>
+        </table>
+      </div>
     </div>
   );
 }
